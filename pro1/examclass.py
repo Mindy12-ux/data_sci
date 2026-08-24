17e01e6b22bb22d8ab0b5170ad9ad1f659b139e1
kmc <min095879@gmail.com>
2026-08-24 15:13:43 +0900

    modified

--- pro1/examclass.py
+++ pro1/examclass.py
@@ -1,34 +1,34 @@
-class CoinIn:   # 자판기에 돈을 넣는 사람 객체, coin, change, cupCount가 왜 있어야 하는지 모르겠음 
+class CoinIn:   # 동전을 넣는 사람 객체
     coin = 0
     change = 0
 
-    def culc(self, coin, cupCount):
+    def culc(self, coin, cupcount):
         self.coin = coin
-        self.cupCount = cupCount
-        self.change = self.coin - 200 * cupCount
+        self.cupcount = cupcount
+        self.change = self.coin - 200 * cupcount
         return self.change
 
-
 class Machine:
+
     def __init__(self):
         self.person = CoinIn()
-    
-    def showdData(self):
-        total_coin = int(input("동전을 입력하세요 : "))
-        total_cupCount = int(input("몇 잔을 원하세요 : "))
 
-        total_count = self.person.culc(total_coin, total_cupCount)
-        
-        if total_count > 0 :
-            print(f"커피 {total_cupCount}잔과 잔돈{total_count}원")
-        
+    def showData(self):
+        total_coin = int(input("동전을 입력하세요 : "))
+        total_cupcount = int(input("몇 잔을 원하세요 : "))
+
+        total_change = self.person.culc(total_coin, total_cupcount)
+
+        if total_change >= 0:
+            print(f"커피 {total_cupcount}잔과 잔돈 {total_change}원")
+
         else:
             print("요금이 부족합니다.")
 
 
 if __name__ == "__main__":
-    coffeemachine1 = Machine()
-    coffeemachine1.showdData()
+    coffeemachine = Machine()
+    coffeemachine.showData()
 
 
 
--- pro1/realprac.py
+++ pro1/realprac.py
@@ -1,34 +1,31 @@
-from test25handle import Handle
+class CoinIn:
+    coin = 0
+    change = 0
 
-class Car:
-    turnShowemessage = "정지"
-    sum_quantity = 0
-    def __init__(self, ownername):
-        self.ownername = ownername
-        self.handle = Handle()  # 클래스의 포함 관계
+    def culc(self, coin, cupcount):
+        self.coin = coin
+        self.cupcount = cupcount
+        self.change = self.coin - 200 * cupcount
+        return self.change
 
+class Machine:
 
-    def turnHandle(self, q):
-        # 회전량 q : 양수면 우회전, 음수면 좌회전, 0이면 직진
-        
-        if q > 0:
-            self.turnShowemessage = self.handle.rigthTurn(q)
-            self.sum_quantity += q
+    def __init__(self):
+        self.person = CoinIn()
 
-        elif q < 0:
-            self.turnShowemessage = self.handle.leftTurn(q)
-            self.sum_quantity += q
+    def showData(self):
+        total_coin = int(input("동전을 입력하세요 : "))
+        total_cupcount = int(input("몇 잔을 원하세요 : "))
 
-        elif q == 0:
-            self.turnShowemessage = "직진"
+        total_change = self.person.culc(total_coin, total_cupcount)
 
+        if total_change >= 0:
+            print(f"커피 {total_cupcount}잔과 잔돈 {total_change}원")
+
+        else:
+            print("요금이 부족합니다.")
 
 
 if __name__ == "__main__":
-    tom = Car("미스터 톰")
-    tom.turnHandle(30)
-    print(tom.ownername + "의 회전량은 " + tom.turnShowemessage + " " + \
-          str(tom.handle.quantity) + ", net quantity는 " + str(tom.sum_quantity))
-    tom.turnHandle(-10)
-    print(tom.ownername + "의 회전량은 " + tom.turnShowemessage + " " + \
-              str(tom.handle.quantity) + ", net quantity는 " + str(tom.sum_quantity))
+    coffeemachine = Machine()
+    coffeemachine.showData()
